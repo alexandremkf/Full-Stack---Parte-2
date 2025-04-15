f7cc2adb8742002364bcc9ec14377a5e670c723b
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-15 15:20:58 -0300

    Fiz o 2.3, no meu cósigo a parte do reduce já estava pronta que fica na const Total, então eu só fiz uma mudança qualquer para poder comitar.

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -22,7 +22,6 @@ const Content = ({ parts }) => {
 
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
   return <p><strong>Total of {total} exercises</strong></p>
 }
 
