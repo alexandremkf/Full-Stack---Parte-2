6e500c1e6fe7fac6555cc66d871afea35d7d750e
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-22 17:55:43 -0300

    Fiz o exercício 2.11, que era pra adicionar a mesma funcionalidade que colocamos no notes, que no caso seria armazenar os contatos usando o “db.json”.

--- introdemo/Phonebook/src/App.jsx
+++ introdemo/Phonebook/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,10 +32,16 @@ const App = () => {
       id: crypto.randomUUID(),
     }
 
-    setPersons(prevPersons => [...prevPersons, personObject])
-
-    setNewName('')
-    setNewNumber('')
+    axios
+    .post('http://localhost:3001/persons', personObject)
+    .then(response => {
+      setPersons(prevPersons => [...prevPersons, response.data])
+      setNewName('')
+      setNewNumber('')
+    })
+    .catch(error => {
+      console.error('Erro ao adicionar pessoa:', error)
+    })
   }
 
   const handlePersonChange = (event) => {
